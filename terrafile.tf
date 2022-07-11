0c3ce33cd6ba8538736030740e0642246174cae2
Rômulo Ferreira <romuloslv@users.noreply.github.com>
2022-07-11 09:31:30 -0300

    fix: remove dependence

--- terrafile.tf
+++ terrafile.tf
@@ -3,6 +3,5 @@ module "instance" {
     subnet_id           = var.subnet_id
     compartment_ocid    = var.compartment_ocid
     configuration       = var.configuration
-    availability_domain = var.availability_domain
     instance_state      = var.instance_state
 }
